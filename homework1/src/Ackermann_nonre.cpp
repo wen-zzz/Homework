63f901bf8fa9f2c5f65bad0af90a4b20a4b9fa60
wen-zzz <wen688345@gmail.com>
2025-10-13 15:42:21 +0800

    Update Ackermann_nonre.cpp

--- homework1/src/Ackermann_nonre.cpp
+++ homework1/src/Ackermann_nonre.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
 int Ack(int m, int n) {
     if (m == 0) return n + 1;
     else if (m == 1) return n + 2;
@@ -24,4 +23,5 @@ int main() {
         cout << "Ackermann(" << m << ", " << n << ") = " << result << endl;
     return 0;
 }
+
 
